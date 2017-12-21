c54e1eabcac06362ccfb978925f8588de05e7da6
Michiel Wouters <michiel_woute64@hotmail.com>
2017-12-21 19:27:07 +0100

    End of backend chapter 1

---
+++ MagicMirror.ConsoleApp/Models/WeatherInformation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MagicMirror.ConsoleApp.Models
+{
+    public class WeatherInformation
+    {
+        public string Location { get; set; }
+
+        public double Temperature { get; set; }
+
+        public string TemperatureUOM { get; set; }
+
+        public string WeatherType { get; set; }
+
+        public string Sunrise { get; set; }
+
+        public string Sunset { get; set; }
+    }
+}
