f7717e13a92407eea36a7b2f7a9700af254b5024
Michiel Wouters <michiel_woute64@hotmail.com>
2018-05-05 22:40:34 +0200

    Added TrafficRepo

---
+++ MagicMirror.DataAccess.Entities/Traffic/TrafficEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MagicMirror.DataAccess.Entities.Traffic
+{
+    public class TrafficEntity
+    {
+    }
+}
---
+++ MagicMirror.DataAccess/Repos/TrafficRepo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using MagicMirror.DataAccess.Entities.Traffic;
+
+namespace MagicMirror.DataAccess.Repos
+{
+    public class TrafficRepo : ITrafficRepo
+    {
+        private string _apiId;
+        private string _apiUrl;
+        private string _url;
+
+        private string _start;
+        private string _destination;
+
+        public async Task<TrafficEntity> GetTrafficInfoAsync(string start, string destination)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
