07ab44cd601cdc5ef10e49b3990263fb4b117d2b
Michiel Wouters <michiel_woute64@hotmail.com>
2018-05-05 12:14:07 +0200

    WIP

--- MagicMirror.Tests/Weather/WeatherDataTests.cs
+++ MagicMirror.Tests/Weather/WeatherDataTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MagicMirror.DataAccess.Entities.Weather;
+using MagicMirror.DataAccess.Repos;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -7,9 +9,22 @@ namespace MagicMirror.Tests.Weather
 {
     public class WeatherDataTests
     {
+        private WeatherRepo _repo;
+
+        public WeatherDataTests()
+        {
+            _repo = new WeatherRepo();
+        }
+
         [Fact]
         public void Can_Retrieve_Weather_Data()
         {
+            // Arrange
+            WeatherEntity entity = null;
+            string city = "London";
+
+            // Act
+            entity = _repo.GetWeatherEntityByCityAsync(city);
         }
     }
 }
