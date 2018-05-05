611a718a64a81b147e8845601400a743644c46bf
Michiel Wouters <michiel_woute64@hotmail.com>
2018-05-05 12:03:20 +0200

    Added Testproject

---
+++ MagicMirror.Tests/Weather/WeatherDataTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MagicMirror.Tests.Weather
+{
+    public class WeatherDataTests
+    {
+        [Fact]
+        public void Can_Retrieve_Weather_Data()
+        {
+        }
+    }
+}
