8885c7090a11fef38f54765b0dcffa9e92702d10
Michiel Wouters <michiel_woute64@hotmail.com>
2018-06-20 16:43:55 +0200

    Added business Models

---
+++ MagicMirror.Business.Models/Enums/TemperatureUom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MagicMirror.Business.Models.Enums
+{
+    public enum TemperatureUom
+    {
+        Kelvin = 0,
+        Fahrenheit = 1,
+        Celsius = 2
+    }
+}
