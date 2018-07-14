7f750f671703e353fde3a9ab843e52ad8411908d
Michiel Wouters <michiel_woute64@hotmail.com>
2018-07-14 18:04:26 +0200

    Adding tests for weather

--- MagicMirror.Tests/Weather/WeatherBusinessTests.cs
+++ MagicMirror.Tests/Weather/WeatherBusinessTests.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using MagicMirror.Business.Services;
+using MagicMirror.Business.Services.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
+using Moq;
+using MagicMirror.DataAccess.Repos;
 
 namespace MagicMirror.Tests.Weather
 {
     public class WeatherBusinessTests
     {
+        private IWeatherService _service;
 
+        private const string Location = "London";
+        private const double Kelvin = 295.15;
+        private const string Weathertype = "Clear";
+        private const string Icon = "01d";
+        private const int Sunrise = 1531281435;
+        private const int Sunset = 1531340063;
+
+        public WeatherBusinessTests()
+        {
+            var mockInterface = new Mock<IWeatherRepo>();
+            _service = new WeatherService(mockInterface.Object);
+        }
     }
 }
