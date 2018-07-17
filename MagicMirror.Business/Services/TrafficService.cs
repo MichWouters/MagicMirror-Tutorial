0dbfe0a27042f602798773aad2f5abd83f3e2467
Michiel Wouters <michiel_woute64@hotmail.com>
2018-07-17 16:54:03 +0200

    Tdd development

---
+++ MagicMirror.Business/Services/TrafficService.cs
@@ -0,0 +1,29 @@
+﻿using MagicMirror.Business.Models;
+using MagicMirror.DataAccess.Entities.Traffic;
+using MagicMirror.DataAccess.Repos;
+using System;
+using System.Threading.Tasks;
+
+namespace MagicMirror.Business.Services
+{
+    public class TrafficService : ITrafficService
+    {
+        private ITrafficRepo _repo;
+
+        public TrafficService(ITrafficRepo repo)
+        {
+            // Dependency Injection
+            _repo = repo;
+        }
+
+        public Task<TrafficModel> GetTrafficModelAsync(string origin, string destination)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TrafficModel MapFromEntity(TrafficEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
