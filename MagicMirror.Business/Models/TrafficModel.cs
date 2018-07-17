689d21215be007359e0d73f5b46c4f03e19a30f2
Michiel Wouters <michiel_woute64@hotmail.com>
2018-07-17 16:37:01 +0200

    Initiating Traffic tests

---
+++ MagicMirror.Business/Models/TrafficModel.cs
@@ -0,0 +1,20 @@
+﻿using MagicMirror.Business.Enums;
+using System;
+
+namespace MagicMirror.Business.Models
+{
+    public class TrafficModel: Model
+    {
+        public string Origin { get; set; }
+
+        public string Destination { get; set; }
+
+        public double Distance { get; set; }
+
+        public int Duration { get; set; }
+
+        public DateTime TimeOfArrival { get; set; }
+
+        public DistanceUom DistanceUom { get; set; }
+    }
+}
---
+++ MagicMirror.Business/Services/ITrafficService.cs
@@ -0,0 +1,13 @@
+﻿using MagicMirror.Business.Models;
+using MagicMirror.DataAccess.Entities.Traffic;
+using System.Threading.Tasks;
+
+namespace MagicMirror.Business.Services
+{
+    public interface ITrafficService
+    {
+        Task<TrafficModel> GetTrafficModelAsync(string origin, string destination);
+
+        TrafficModel MapFromEntity(TrafficEntity entity);
+    }
+}
