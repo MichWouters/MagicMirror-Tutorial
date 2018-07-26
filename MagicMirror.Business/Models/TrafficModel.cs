6630a9289884713604f79ea92bac47da261dd9ae
Michiel Wouters <michiel_woute64@hotmail.com>
2018-07-26 17:50:06 +0200

    Finished Models

--- MagicMirror.Business/Models/TrafficModel.cs
+++ MagicMirror.Business/Models/TrafficModel.cs
@@ -1,11 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MagicMirror.Business.Enums;
+using System;
 
 namespace MagicMirror.Business.Models
 {
-    public class TrafficModel: Model
+    public class TrafficModel : Model
     {
+        public string Origin { get; set; }
 
+        public string Destination { get; set; }
+
+        public double Distance { get; set; }
+
+        public int Duration { get; set; }
+
+        public DateTime TimeOfArrival { get; set; }
+
+        public DistanceUom DistanceUom { get; set; }
     }
 }
--- MagicMirror.Business/Models/WeatherModel.cs
+++ MagicMirror.Business/Models/WeatherModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MagicMirror.Business.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
