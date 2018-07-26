54e401336e25cbaee97b45b433004ba55efd9623
Michiel Wouters <michiel_woute64@hotmail.com>
2018-07-27 00:40:47 +0200

    Added AutoMapperConfiguration

---
+++ MagicMirror.Business/Configuration/AutoMapperConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MagicMirror.Business.Models;
+using MagicMirror.DataAccess.Entities.Weather;
+
+namespace MagicMirror.Business.Configuration
+{
+    public class AutoMapperConfiguration: Profile
+    {
+        public AutoMapperConfiguration()
+        {
+            CreateMap<WeatherEntity, WeatherModel>()
+                .ForMember(x=> x.Location, y => y.MapFrom(z => z.Name))
+                .ForMember(x => x.Location, y => y.MapFrom(z => z.Name))
+                .ForMember(x => x.Sunrise, y => y.MapFrom(z => z.Sys.Sunrise))
+                .ForMember(x => x.Sunset, y => y.MapFrom(z => z.Sys.Sunset))
+                .ForMember(x => x.WeatherType, y => y.MapFrom(z => z.Weather[0].Main))
+                .ForMember(x => x.Icon, y => y.MapFrom(z => z.Weather[0].Icon));
+        }
+    }
+}
