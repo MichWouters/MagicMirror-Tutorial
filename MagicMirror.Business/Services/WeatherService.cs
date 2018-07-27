736f94a85026c847a5ab1bf4134e2c747810a705
Michiel Wouters <michiel_woute64@hotmail.com>
2018-07-27 13:09:04 +0200

    Basic WeatherMapping

---
+++ MagicMirror.Business/Services/WeatherService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using AutoMapper.Configuration;
+using MagicMirror.Business.Configuration;
+using MagicMirror.Business.Models;
+using MagicMirror.DataAccess.Entities.Weather;
+using MagicMirror.DataAccess.Repos;
+
+namespace MagicMirror.Business.Services
+{
+    public class WeatherService
+    {
+        private IMapper mapper;
+        private IWeatherRepo _repo;
+
+        public WeatherService()
+        {
+            SetUpMapperConfig();
+        }
+
+        private void SetUpMapperConfig()
+        {
+            var baseMappings = new MapperConfigurationExpression();
+            baseMappings.AddProfile<AutoMapperConfiguration>();
+            var config = new MapperConfiguration(baseMappings);
+
+            mapper = new Mapper(config);
+        }
+
+        public WeatherModel MapFromEntity(WeatherEntity entity)
+        {
+            var model = mapper.Map<WeatherModel>(entity);
+            return model;
+        }
+    }
+}
