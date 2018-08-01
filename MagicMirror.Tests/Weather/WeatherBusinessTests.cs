9d2e192a02a49d031b74a254d149690a47b5870c
Michiel Wouters <michiel_woute64@hotmail.com>
2018-08-01 15:50:52 +0200

    Commencing testing

--- MagicMirror.Tests/Weather/WeatherBusinessTests.cs
+++ MagicMirror.Tests/Weather/WeatherBusinessTests.cs
@@ -3,11 +3,55 @@ using System.Collections.Generic;
 using System.Text;
 using Xunit;
 using MagicMirror.Business.Services;
+using MagicMirror.DataAccess.Entities.Weather;
 
 namespace MagicMirror.Tests.Weather
 {
     public class WeatherBusinessTests
     {
         private IWeatherService _service;
+
+        // Mock values
+        private const string Location = "London";
+        private const float Kelvin = 295.15f;
+        private const string Weathertype = "Clear";
+        private const string Icon = "01d";
+        private const int Sunrise = 1531281435;
+        private const int Sunset = 1531340063;
+
+        public WeatherBusinessTests()
+        {
+            _service = new WeatherService();
+        }
+
+        private WeatherEntity GetMockEntity()
+        {
+            var main = new Main
+            {
+                Temp = Kelvin
+            };
+
+            var sys = new Sys
+            {
+                Sunrise = Sunrise,
+                Sunset = Sunset
+            };
+
+            var weather = new DataAccess.Entities.Weather.Weather
+            {
+                Main = Weathertype,
+                Icon = Icon
+            };
+
+            var weatherEntity = new WeatherEntity
+            {
+                Name = Location,
+                Main = main,
+                Sys = sys,
+                Weather = new[] { weather }
+            };
+
+            return weatherEntity;
+        }
     }
 }
