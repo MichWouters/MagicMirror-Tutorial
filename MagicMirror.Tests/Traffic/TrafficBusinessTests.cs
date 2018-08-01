62d00cf78e6bb89172284fb888ca6ad939fba317
Michiel Wouters <michiel_woute64@hotmail.com>
2018-08-02 00:49:42 +0200

    Starting TrafficTests

---
+++ MagicMirror.Tests/Traffic/TrafficBusinessTests.cs
@@ -0,0 +1,17 @@
+﻿using MagicMirror.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MagicMirror.Tests.Traffic
+{
+    public class TrafficBusinessTests
+    {
+        private ITrafficService _service;
+
+        public TrafficBusinessTests()
+        {
+
+        }
+    }
+}
