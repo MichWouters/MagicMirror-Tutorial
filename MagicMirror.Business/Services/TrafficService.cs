6207040032b9e6a3ee5b3edf96d6b159e7ec2288
Michiel Wouters <michiel_woute64@hotmail.com>
2018-08-02 01:24:05 +0200

    Updating code

---
+++ MagicMirror.Business/Services/TrafficService.cs
@@ -0,0 +1,28 @@
+﻿using MagicMirror.Business.Models;
+using MagicMirror.DataAccess.Entities.Traffic;
+using MagicMirror.DataAccess.Repos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicMirror.Business.Services
+{
+    public class TrafficService : MappableService<TrafficEntity, TrafficModel>, ITrafficService
+    {
+        private ITrafficRepo _repo;
+
+        public TrafficService()
+        {
+            _repo = new TrafficRepo();
+        }
+
+        public async Task<TrafficModel> GetTrafficModelAsync(string origin, string destination)
+        {
+            TrafficEntity entity = await _repo.GetTrafficInfoAsync(origin, destination);
+            TrafficModel model = MapFromEntity(entity);
+
+            return model;
+        }
+    }
+}
--- MagicMirror.Tests/Traffic/TrafficBusinessTests.cs
+++ MagicMirror.Tests/Traffic/TrafficBusinessTests.cs
@@ -1,7 +1,10 @@
-﻿using MagicMirror.Business.Services;
+﻿using MagicMirror.Business.Models;
+using MagicMirror.Business.Services;
+using MagicMirror.DataAccess.Entities.Traffic;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace MagicMirror.Tests.Traffic
 {
@@ -9,9 +12,55 @@ namespace MagicMirror.Tests.Traffic
     {
         private ITrafficService _service;
 
+        // Mock Data
+        private const int Duration = 42;
+        private const int Distance = 76;
+        private const string Origin = "London, Uk";
+        private const string Destination = "Leeds, Uk";
+
         public TrafficBusinessTests()
         {
+            _service = new TrafficService();
+        }
 
+        [Fact]
+        public void Can_Map_From_Entity()
+        {
+            // Arrange
+            TrafficEntity entity = GetMockEntity();
+
+            // Act
+            TrafficModel model = _service.MapFromEntity(entity);
+
+            // Assert
+            Assert.Equal(Distance, model.Distance);
+            Assert.Equal(Duration, model.Duration);
+            Assert.Equal(Destination, model.Destination);
+            Assert.Equal(Origin, model.Origin);
+        }
+
+        private TrafficEntity GetMockEntity()
+        {
+            var element = new Element
+            {
+                Distance = new Distance { Value = Distance },
+                Duration = new Duration { Value = Duration }
+            };
+
+            var entity = new TrafficEntity
+            {
+                Origin_addresses = new[] { Origin },
+                Destination_addresses = new[] { Destination },
+                Rows = new[]
+                {
+                    new Row
+                    {
+                        Elements = new [] { element }
+                    }
+                }
+            };
+
+            return entity;
         }
     }
 }
