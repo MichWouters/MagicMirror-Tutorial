3075206353eff394955100f4e8a3bac8fe53c7fb
Michiel Wouters <michiel_woute64@hotmail.com>
2018-08-14 23:49:44 +0200

    Added DistanceTests

---
+++ Acme.Generic.Tests/DistanceTests.cs
@@ -0,0 +1,45 @@
+﻿using Acme.Generic.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Acme.Generic.Tests
+{
+    public class DistanceTests
+    {
+        [Fact]
+        public void Miles_To_Kilometres()
+        {
+            // Arrange
+            double[] inputs = { 1, 25, 37.33 };
+            double[] expectedValues = { 1.61, 40.23, 60.08 };
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                // Act
+                double result = DistanceHelper.MilesToKilometers(inputs[i]);
+
+                // Assert
+                Assert.Equal(result, expectedValues[i]);
+            }
+        }
+
+        [Fact]
+        public void KiloMetres_To_Miles()
+        {
+            // Arrange
+            double[] inputs = { 1, 25, 37.33 };
+            double[] expectedValues = { 0.62, 15.53, 23.2 };
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                // Act
+                double result = DistanceHelper.MilesToKilometers(inputs[i]);
+
+                // Assert
+                Assert.Equal(result, expectedValues[i]);
+            }
+        }
+    }
+}
---
+++ Acme.Generic/Helpers/DistanceHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Acme.Generic.Helpers
+{
+    public static class DistanceHelper
+    {
+        public static double MilesToKilometers(double valueToConvert, int precision = 2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double KilometersToMiles(double valueToConvert, int precision = 2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
