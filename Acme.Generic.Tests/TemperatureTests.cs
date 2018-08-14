562d44e9dd50057d5fc7f5cbbe4ef62b4d429537
Michiel Wouters <michiel_woute64@hotmail.com>
2018-08-14 21:09:01 +0200

    Updated packages

---
+++ Acme.Generic.Tests/TemperatureTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acme.Generic.Tests
+{
+    public class TemperatureTests
+    {
+    }
+}
---
+++ Acme.Generic/Helpers/TemperatureHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acme.Generic.Helpers
+{
+    public static class TemperatureHelper
+    {
+        public static object KelvinToCelsius(double valueToConvert)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static object CelsiusToKelvin(double valueToConvert)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
