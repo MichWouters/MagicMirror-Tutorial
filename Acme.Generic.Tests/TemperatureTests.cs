8d76f2d6b57328713d97753db8faa1e73cddd593
Michiel Wouters <michiel_woute64@hotmail.com>
2018-08-14 23:18:47 +0200

    Implemented TemperatureHelper

--- Acme.Generic.Tests/TemperatureTests.cs
+++ Acme.Generic.Tests/TemperatureTests.cs
@@ -6,29 +6,51 @@ namespace Acme.Generic.Tests
     public class TemperatureTests
     {
         [Fact]
-        public void Can_Convert_Kelvin_To_Celsius()
+        public void Kelvin_To_Celsius()
         {
-            // Arrange
             double toConvert = 293.15;
-
-            // Act
             double convertedValue = TemperatureHelper.KelvinToCelsius(toConvert);
-
-            // Assert
             Assert.Equal(20, convertedValue);
         }
 
         [Fact]
-        public void Can_Convert_Celsius_To_Kelvin()
+        public void Kelvin_To_Fahrenheit()
         {
-            // Arrange
+            double toConvert = 293.15;
+            double convertedValue = TemperatureHelper.KelvinToFahrenheit(toConvert);
+            Assert.Equal(68, convertedValue);
+        }
+
+        [Fact]
+        public void Celsius_To_Kelvin()
+        {
             double toConvert = 20;
-
-            // Act
             double convertedValue = TemperatureHelper.CelsiusToKelvin(toConvert);
-
-            // Assert
             Assert.Equal(293.15, convertedValue);
+        }
+
+        [Fact]
+        public void Celsius_To_Fahrenheit()
+        {
+            double toConvert = 20;
+            double convertedValue = TemperatureHelper.CelsiusToFahrenheit(toConvert);
+            Assert.Equal(68, convertedValue);
+        }
+
+        [Fact]
+        public void Fahrenheit_To_Kelvin()
+        {
+            double toConvert = 79.25;
+            double convertedValue = TemperatureHelper.FahrenheitToKelvin(toConvert);
+            Assert.Equal(299.4, convertedValue);
+        }
+
+        [Fact]
+        public void Fahrenheit_To_Celsius()
+        {
+            double toConvert = 79.25;
+            double convertedValue = TemperatureHelper.FahrenheitToCelsius(toConvert);
+            Assert.Equal(26.25, convertedValue);
         }
     }
 }
--- Acme.Generic/Helpers/TemperatureHelper.cs
+++ Acme.Generic/Helpers/TemperatureHelper.cs
@@ -15,5 +15,29 @@ namespace Acme.Generic.Helpers
             double converted = valueToConvert + 273.15;
             return Math.Round(converted, precision);
         }
+
+        public static double KelvinToFahrenheit(double valueToConvert, int precision = 2)
+        {
+            double converted = 1.8 * (valueToConvert - 273.15) + 32;
+            return Math.Round(converted, precision);
+        }
+
+        public static double CelsiusToFahrenheit(double valueToConvert, int precision = 2)
+        {
+            double converted = (valueToConvert * 1.8) + 32;
+            return Math.Round(converted, precision);
+        }
+
+        public static double FahrenheitToKelvin(double valueToConvert, int precision = 2)
+        {
+            double converted = (5f / 9f) * (valueToConvert - 32) + 273.15;
+            return Math.Round(converted, precision);
+        }
+
+        public static double FahrenheitToCelsius(double valueToConvert, int precision = 2)
+        {
+            double converted = (valueToConvert - 32) / 1.8;
+            return Math.Round(converted, precision);
+        }
     }
 }
