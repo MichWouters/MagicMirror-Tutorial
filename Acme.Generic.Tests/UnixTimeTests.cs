d2a6a62d0bfa48e5532685ab8d66c629af445c54
Michiel Wouters <michiel_woute64@hotmail.com>
2018-08-25 11:54:01 +0200

    Adding UnixTimeTests

--- Acme.Generic.Tests/UnixTimeTests.cs
+++ Acme.Generic.Tests/UnixTimeTests.cs
@@ -1,44 +1,15 @@
-﻿using Acme.Generic.Helpers;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using Xunit;
 
 namespace Acme.Generic.Tests
 {
     public class UnixTimeTests
     {
-        private int sunrise = 1534400420; //16 August 2018 06:20:20
-        private int sunset = 1534454400; // 16 August 2018 21:20:00
+        private int unixSunrise = 1535189400; // 25 August 2018 09:30:00
+        private int unixSunset = 1535232600; // 25 August 2018 21:30:00
 
-        [Fact]
-        public void Convert_From_Unix()
-        {
-            // Arrange
-            DateTime expectedSunrise = new DateTime(2018, 8, 16, 6, 20, 20);
-            DateTime expectedSunset = new DateTime(2018, 8, 16, 21, 20, 00);
 
-            // Act
-            DateTime convertedSunrise = UnixTimeHelper.ConvertFromUnixTimeStamp(sunrise);
-            DateTime convertedSunset = UnixTimeHelper.ConvertFromUnixTimeStamp(sunset);
-
-            // Assert
-            Assert.Equal(expectedSunrise, convertedSunrise);
-            Assert.Equal(expectedSunset, convertedSunset);
-        }
-
-        [Fact]
-        public void Convert_To_Unix()
-        {
-            // Arrange
-            DateTime sunriseDate = new DateTime(2018, 8, 16, 6, 20, 20);
-            DateTime sunsetDate = new DateTime(2018, 8, 16, 21, 20, 00);
-
-            // Act
-            int convertedSunrise = UnixTimeHelper.ConvertToUnixTimeStamp(sunriseDate);
-            int convertedSunset = UnixTimeHelper.ConvertToUnixTimeStamp(sunsetDate);
-
-            // Assert
-            Assert.Equal(sunrise, convertedSunrise);
-            Assert.Equal(sunset, convertedSunset);
-        }
     }
 }
---
+++ Acme.Generic/Helpers/DateTimeHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Acme.Generic.Helpers
+{
+    public static class DateTimeHelper
+    {
+        private static DateTime baseUnixTime =
+            new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static DateTime ConvertUnixTimeToGMTDateTime(int timeStamp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int ConvertGMTDateTimeToUnixTime(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
