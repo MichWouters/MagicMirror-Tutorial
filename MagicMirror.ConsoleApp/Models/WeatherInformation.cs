51815340cfa0720115c663d6c88885915c895158
Michiel Wouters <michiel_woute64@hotmail.com>
2019-02-08 19:14:58 +0100

    Rollbacking some changes

---
+++ MagicMirror.ConsoleApp/Models/WeatherInformation.cs
@@ -0,0 +1,17 @@
+﻿namespace MagicMirror.ConsoleApp.Models
+{
+    public class WeatherInformation
+    {
+        public string Location { get; set; }
+
+        public double Temperature { get; set; }
+
+        public string TemperatureUOM { get; set; }
+
+        public string WeatherType { get; set; }
+
+        public string Sunrise { get; set; }
+
+        public string Sunset { get; set; }
+    }
+}
