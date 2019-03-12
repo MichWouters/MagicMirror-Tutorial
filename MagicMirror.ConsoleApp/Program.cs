2b1e7c3cc6ba7468485f103cbd1a9a18e7306565
Michiel Wouters <michiel_woute64@hotmail.com>
2019-03-12 19:39:34 +0100

    Rollbacking changes to match with tutorials

--- MagicMirror.ConsoleApp/Program.cs
+++ MagicMirror.ConsoleApp/Program.cs
@@ -1,47 +1,11 @@
-﻿using AutoMapper;
-using MagicMirror.Business.Configuration;
-using MagicMirror.Business.Services;
-using MagicMirror.ConsoleApp.Configuration;
-using MagicMirror.DataAccess.Repos;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace MagicMirror.ConsoleApp
+﻿namespace MagicMirror.ConsoleApp
 {
-    public class Program
+    internal class Program
     {
-        public static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            ServiceCollection services = new ServiceCollection();
-            RegisterServices(services);
-            RegisterAutoMapper();
-
-            ServiceProvider provider = services.BuildServiceProvider();
-            provider.GetService<MagicMirrorApp>().RunAsync().GetAwaiter().GetResult();
-        }
-
-        private static void RegisterServices(ServiceCollection services)
-        {
-            // Add Services using Dependency Injection
-            services.AddTransient<ITrafficService, TrafficService>();
-            services.AddTransient<ITrafficRepo, TrafficRepo>();
-
-            services.AddTransient<IWeatherService, WeatherService>();
-            services.AddTransient<IWeatherRepo, WeatherRepo>();
-
-            // Register App
-            services.AddTransient<MagicMirrorApp>();
-
-            // Register AutoMapper
-            services.AddAutoMapper();
-        }
-
-        private static void RegisterAutoMapper()
-        {
-            Mapper.Initialize(cfg =>
-            {
-                cfg.AddProfile<AutoMapperBusinessProfile>();
-                cfg.AddProfile<AutoMapperPresentationProfile>();
-            });
+            var main = new Main();
+            main.Run();
         }
     }
 }
