5192cf2efb2dd8a03d9f9675ba0c31cca966885d
Michiel Wouters <michiel_woute64@hotmail.com>
2019-03-13 19:37:31 +0100

    Fix time issue

--- MagicMirror.ConsoleApp/Converters/TrafficModelToMainViewModel.cs
+++ MagicMirror.ConsoleApp/Converters/TrafficModelToMainViewModel.cs
@@ -10,12 +10,43 @@ namespace MagicMirror.ConsoleApp.Converters
     {
         public MainViewModel Convert(TrafficModel source, MainViewModel destination, ResolutionContext context)
         {
+            // Converter classes cannot automap properties...
+            destination.Distance = source.Distance;
+
+            // But they do allow calculations to be performed at map-time.
+            destination.DistanceUom = GetDistanceUomToString(source.DistanceUom);
             destination.TimeOfArrival = source.TimeOfArrival.ToLocalTime().ToShortTimeString();
             destination.TravelTime = GetHoursAndMinutes(source.Duration);
 
             return destination;
         }
 
-      
+        private string GetDistanceUomToString(DistanceUom distanceUom)
+        {
+            if (distanceUom == DistanceUom.Imperial)
+            {
+                return "miles";
+            }
+            else
+            {
+                return "kilometers";
+            }
+        }
+
+        private string GetHoursAndMinutes(int seconds)
+        {
+            int minutes = seconds / 60;
+            int hours = minutes / 60;
+            int remainingMinutes = minutes % 60;
+
+            if (hours > 0)
+            {
+                return $"{hours} hours and {remainingMinutes} minutes";
+            }
+            else
+            {
+                return $"{remainingMinutes} minutes";
+            }
+        }
     }
 }
