8b078ed97250c491ab2b8448580935631f366330
Michiel Wouters <michiel_woute64@hotmail.com>
2019-03-13 19:34:02 +0100

    Rollback

--- MagicMirror.ConsoleApp/Converters/TrafficModelToMainViewModel.cs
+++ MagicMirror.ConsoleApp/Converters/TrafficModelToMainViewModel.cs
@@ -10,42 +10,12 @@ namespace MagicMirror.ConsoleApp.Converters
     {
         public MainViewModel Convert(TrafficModel source, MainViewModel destination, ResolutionContext context)
         {
-            // Converter classes cannot automap properties...
-            destination.Distance = source.Distance;
-
-            // But they do allow calculations to be performed at map-time.
-            destination.DistanceUom = GetDistanceUomToString(source.DistanceUom);
             destination.TimeOfArrival = source.TimeOfArrival.ToLocalTime().ToShortTimeString();
             destination.TravelTime = GetHoursAndMinutes(source.Duration);
 
             return destination;
         }
 
-        private string GetDistanceUomToString(DistanceUom distanceUom)
-        {
-            if (distanceUom == DistanceUom.Imperial)
-            {
-                return "miles";
-            }
-            else
-            {
-                return "kilometers";
-            }
-        }
-
-        private string GetHoursAndMinutes(int duration)
-        {
-            int hours = duration / 60;
-            int minutes = duration % 60;
-
-            if (hours > 0)
-            {
-                return $"{hours} hours and {minutes} minutes";
-            }
-            else
-            {
-                return $"{minutes} minutes";
-            }
-        }
+      
     }
 }
