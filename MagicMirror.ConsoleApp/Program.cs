c56091cef7dd854d2a9ed0ff2beac8469ac3d352
Michiel Wouters <mwouters@W3008>
2019-03-22 16:22:05 +0100

    Code at end of backend-chapter-8

--- MagicMirror.ConsoleApp/Program.cs
+++ MagicMirror.ConsoleApp/Program.cs
@@ -1,31 +1,15 @@
-﻿namespace MagicMirror.ConsoleApp
+﻿using AutoMapper;
+using MagicMirror.Business.Configuration;
+using MagicMirror.ConsoleApp.Configuration;
+
+namespace MagicMirror.ConsoleApp
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            IServiceCollection services = new ServiceCollection();
-            RegisterServices(services);
-            RegisterAutoMapper();
-
-            ServiceProvider provider = services.BuildServiceProvider();
-            provider.GetService<MagicMirrorApp>().RunAsync().GetAwaiter().GetResult();
-        }
-
-        private static void RegisterServices(IServiceCollection services)
-        {
-            // Add Services using Dependency Injection
-            services.AddTransient<ITrafficService, TrafficService>();
-            services.AddTransient<ITrafficRepo, TrafficRepo>();
-
-            services.AddTransient<IWeatherService, WeatherService>();
-            services.AddTransient<IWeatherRepo, WeatherRepo>();
-
-            // Register App
-            services.AddTransient<MagicMirrorApp>();
-
-            // Register AutoMapper
-            services.AddAutoMapper();
+            var main = new Main();
+            main.RunAsync();
         }
 
         private static void RegisterAutoMapper()
