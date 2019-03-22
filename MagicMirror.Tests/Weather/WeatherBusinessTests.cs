9dc45ddb633245e7663787dbafaf7010fd96bbe3
Michiel Wouters <mwouters@W3008>
2019-03-22 16:36:23 +0100

    Fix unit tests

--- MagicMirror.Tests/Weather/WeatherBusinessTests.cs
+++ MagicMirror.Tests/Weather/WeatherBusinessTests.cs
@@ -26,14 +26,13 @@ namespace MagicMirror.Tests.Weather
         }
 
         [Fact]
-        public void Calculate_DateTimes_Correctly()
+        public async Task Calculate_DateTimes_Correctly()
         {
             // Arrange
             WeatherEntity entity = GetMockEntity();
 
             // Act
-            WeatherModel model = _service.MapFromEntity(entity);
-            model.InitializeModel();
+            WeatherModel model = await _service.GetWeatherModelAsync(Location);
 
             // Assert
             Assert.Equal("01:01", model.Sunrise);
@@ -41,13 +40,13 @@ namespace MagicMirror.Tests.Weather
         }
 
         [Fact]
-        public void Calculate_Temperatures_Correctly()
+        public async Task Calculate_Temperatures_CorrectlyAsync()
         {
             // Arrange
             WeatherEntity entity = GetMockEntity();
 
             // Act
-            WeatherModel model = _service.MapFromEntity(entity);
+            WeatherModel model = await _service.GetWeatherModelAsync(Location);
             double celsius = model.ConvertTemperature(TemperatureUom.Celsius);
             double fahrenheit = model.ConvertTemperature(TemperatureUom.Fahrenheit);
             double kelvin = model.ConvertTemperature(TemperatureUom.Kelvin);
@@ -56,24 +55,6 @@ namespace MagicMirror.Tests.Weather
             Assert.Equal(22, celsius);
             Assert.Equal(71.6, fahrenheit);
             Assert.Equal(295.15, kelvin);
-        }
-
-        [Fact]
-        public void Can_Map_From_Entity()
-        {
-            // Arrange
-            WeatherEntity entity = GetMockEntity();
-
-            // Act
-            WeatherModel model = _service.MapFromEntity(entity);
-
-            // Assert
-            Assert.Equal(Location, model.Location);
-            Assert.Equal(Weathertype, model.WeatherType);
-            Assert.Equal(Kelvin, model.Temperature);
-            Assert.Equal(Sunrise.ToString(), model.Sunrise);
-            Assert.Equal(Sunset.ToString(), model.Sunset);
-            Assert.Equal(Icon, model.Icon);
         }
 
         private WeatherEntity GetMockEntity()
