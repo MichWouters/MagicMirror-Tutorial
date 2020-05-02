3360715e7ea5c14af92646aa4648534ef0b31c7b
Michiel Wouters <michiel_woute64@hotmail.com>
2020-05-02 21:39:58 +0200

    4. Update AutoMapper

--- MagicMirror.Business/Configuration/AutoMapperBusinessProfile.cs
+++ MagicMirror.Business/Configuration/AutoMapperBusinessProfile.cs
@@ -1,6 +1,8 @@
-﻿using Acme.Generic.Helpers;
+﻿using System.ComponentModel.Design;
+using Acme.Generic.Helpers;
 using AutoMapper;
 using MagicMirror.Business.Models;
+using MagicMirror.DataAccess.Entities.Entities;
 using MagicMirror.DataAccess.Entities.Traffic;
 using MagicMirror.DataAccess.Entities.Weather;
 
@@ -18,11 +20,19 @@ namespace MagicMirror.Business.Configuration
                 .ForMember(x => x.WeatherType, y => y.MapFrom(z => z.Weather[0].Main))
                 .ForMember(x => x.Icon, y => y.MapFrom(z => z.Weather[0].Icon));
 
-            CreateMap<GoogleMapsTrafficEntity, GoogleMapsTrafficModel>()
+            CreateMap<GoogleMapsTrafficEntity, TrafficModel>()
                 .ForMember(x => x.Destination, y => y.MapFrom(z => z.Destination_addresses[0]))
                 .ForMember(x => x.Origin, y => y.MapFrom(z => z.Origin_addresses[0]))
                 .ForMember(x => x.Distance, y => y.MapFrom(z => z.Rows[0].Elements[0].Distance.Value))
+                .ForMember(x => x.DistanceUom, y => y.MapFrom(z => Enums.DistanceUom.Metric))
                 .ForMember(x => x.Duration, y => y.MapFrom(z => z.Rows[0].Elements[0].Duration.Value));
-        }
+
+
+            CreateMap<OpenMapTraffic, TrafficModel>()
+                .ForMember(x => x.Distance, y => y.MapFrom(z => z.Route.Distance))
+                .ForMember(x => x.DistanceUom, y => y.MapFrom(z => Enums.DistanceUom.Imperial))
+                .ForMember(x => x., y => y.MapFrom(z => Enums.DistanceUom.Imperial))
+                .ForMember(x => x.Duration, y => y.MapFrom(z => z.Route.Time));
+        }   
     }
 }
